e913f5bf6b24c47911cb07aabccf253d0d2efee7
Areebah-snow <mudathiraisha657@gmail.com>
2023-05-31 07:35:35 +0100

    Facing  javascript

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -1,9 +1,18 @@
 let mainDiv = document.querySelector(".inner-container")
 let Btn = document.querySelector(".btn");
 let imageDiv = document.createElement("div")
-let ratingDiv =  document.createElement("div");
+let ratingPg =  document.createElement("p")
+const bodyElements = document.querySelectorAll('body *');
+let headerDiv = document.createElement('h2')
+ratingPg = "You have selected 4 out of 5"
+headerDiv = `<h2>Thank you</h2>`
+ratingPg.classList.add("rating-div")   
 imageDiv = `<img src="images/illustration-thank-you.svg" alt="">`
+
+
 Btn.addEventListener('click', function(){
-    mainDiv.innerHTML = imageDiv
+    mainDiv.innerHTML = `${imageDiv}<br> ${ratingPg} <br>
+     ${headerDiv}
+    `
     console.log("okay")
 });
